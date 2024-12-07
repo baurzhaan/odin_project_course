f457531eb4b1dd028d21789b2e6f0a4e14e0454f
Baurzhaan <102077363+baurzhaan@users.noreply.github.com>
2024-12-07 22:48:56 +0000

    complete task 2

--- substrings.rb
+++ substrings.rb
@@ -12,23 +12,10 @@ Implement a method #substrings that takes a word as the first argument and then 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(string, dictionary)
-  p string
-  p dictionary
-  # puts "below => 1, low => 1"
-  puts '++++++++++++++++'
-
-  result = Hash.new(0)
-
-  dictionary.each do |substring|
-    if string.downcase.include?(substring)
-      result[substring] += 1
-    end
-  end
-
-
-  result
+  result = {}
+  dictionary.map { |substring| result[substring] = string.downcase.scan(substring).size }
+  result.select { |_, count| count != 0 }
 end
 
-p substrings('below', dictionary)
-# p substrings("Howdy partner, sit down! How's it going?", dictionary)
-# puts '{ "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }'
+p substrings("below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
